a8f4d6092b886451d682d55329bd47a1ca47fa75
ackman678 <james.ackman@gmail.com>
2019-06-20 20:38:14 -0700

    ext display type

--- hdmi.sh
+++ hdmi.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-#setup external hdmi display if connected, else ensure the display is off
+if [ "$1" == "-h" ] ; then
+    echo "
+          hdmi - setup external display if connected, else ensure the display is off
+               - run 'xrandr' to see display interface names for input
+    
+              Usage: hdmi.sh 
+                     hdmi.sh DP1 
+    "
+    exit 0
+fi
+
+set -e
+
 intern=eDP1
-extern=HDMI1
+extern=${1:-HDMI1}
 
 if xrandr | grep "$extern connected"; then
     xrandr --output "$intern" --auto --output "$extern" --auto --right-of "$intern"
