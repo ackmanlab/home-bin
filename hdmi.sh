69c3270c515fe8a4d887a81bcc914184875bd451
ackman678 <james.ackman@gmail.com>
2020-05-10 22:25:36 -0700

    working wayland

--- hdmi.sh
+++ hdmi.sh
@@ -3,6 +3,7 @@ if [ "$1" == "-h" ] ; then
     echo "
           hdmi - setup external display if connected, else ensure the display is off
                - run 'xrandr' to see display interface names for input
+               - use for X11 window managers such as i3wm
     
               Usage: hdmi.sh 
                      hdmi.sh DP1 
--- mv2figs.sh
+++ mv2figs.sh
@@ -3,7 +3,9 @@
 
 #default image location
 blobFolder=$HOME/figures
+# wayland: use "wl-copy"
+#xorg-x11: use "xclip -selection clipboard"
 
 mv $1 $blobFolder/$1
 
-echo "<figure><img src=\""$(basename $blobFolder)/$1"\" width=\"500px\"><figcaption></figcaption></figure>" | xclip -selection clipboard
+echo "<figure><img src=\""$(basename $blobFolder)/$1"\" width=\"500px\"><figcaption></figcaption></figure>" | wl-copy 
